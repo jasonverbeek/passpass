e7c31424ecf0d1baae2105305add149152be47d2
Jason Verbeek <jason@localhost8080.org>
2023-05-24 08:26:13 +0200

    add option for --no-clear and --confirm (press return)

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,31 @@
 use arboard::Clipboard;
+use clap::Parser;
 use enigo::*;
 use std::{process, thread::sleep, time::Duration};
 
+#[derive(Debug, Parser)]
+struct Args {
+    #[arg(short, long, help = "do NOT clear the clipboard at the end")]
+    no_clear: bool,
+    #[arg(short, long, help = "confirm password by automatically hitting Return")]
+    confirm: bool,
+}
+
 fn main() {
+    let args = Args::parse();
     let mut enigo = Enigo::new();
     let mut clip = match Clipboard::new() {
         Ok(c) => c,
         Err(_) => {
             eprintln!("Access to clipboard denied");
-            process::exit(1)
+            process::exit(1);
         }
     };
     let pass = match clip.get_text() {
         Ok(p) => p,
         Err(_) => {
             eprintln!("Clipboard doesn't contain text");
-            process::exit(1)
+            process::exit(1);
         }
     };
     println!("Typing password from clipboard in 3s...");
@@ -24,9 +34,15 @@ fn main() {
         eprintln!("Failed to send keystrokes to system");
         process::exit(1);
     }
-    if let Err(_) = clip.clear() {
-        eprintln!("Failed to clear the clipboard.");
-        process::exit(1);
+    if args.confirm {
+        sleep(Duration::from_millis(100));
+        enigo.key_click(enigo::Key::Return);
     }
-    println!("Clipboard cleared");
+    if !args.no_clear {
+        if let Err(_) = clip.clear() {
+            eprintln!("Failed to clear the clipboard.");
+            process::exit(1);
+        }
+        println!("Clipboard cleared");
+    }
 }
